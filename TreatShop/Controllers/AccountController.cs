a25803846952d423cfdb15cfd6247178619f5246
Keturah Howard <keturah.dev@gmail.com>
2020-03-27 14:33:29 -0700

    create functionality to log in a registered user

--- TreatShop/Controllers/AccountController.cs
+++ TreatShop/Controllers/AccountController.cs
@@ -28,5 +28,39 @@ namespace TreatShop.Controllers
     {
       return View();
     }
+
+    [HttpPost]
+    public async Task<ActionResult> Register(RegisterViewModel model)
+    {
+      var user = new ApplicationUser { UserName = model.Email };
+      IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+      if (result.Succeeded)
+      {
+        return RedirectToAction("Login");
+      }
+      else
+      {
+        return View();
+      }
+    }
+
+    public ActionResult Login()
+    {
+      return View();
+    }
+    
+    [HttpPost]
+    public async Task<ActionResult> Login(LoginViewModel model)
+    {
+      Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: true, lockoutOnFailure: false);
+      if (result.Succeeded)
+      {
+        return RedirectToAction("Index");
+      }
+      else
+      {
+        return View();
+      }
+    }
   }
 }
