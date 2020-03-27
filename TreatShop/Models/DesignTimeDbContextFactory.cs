fef85defe72c849c11fe0e106d1ec805a50cfcd7
Keturah Howard <keturah.dev@gmail.com>
2020-03-27 09:17:38 -0700

    create configuration for migrations to be built referencing TreatShopContext

---
+++ TreatShop/Models/DesignTimeDbContextFactory.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace TreatShop.Models
+{
+  public class TreatShopContextFactory : IDesignTimeDbContextFactory<TreatShopContext>
+  {
+    TreatShopContext IDesignTimeDbContextFactory<TreatShopContext>.CreateDbContext(string[] arg)
+    {
+      IConfigurationRoot configuration = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json")
+        .Build();
+
+      var builder = new DbContextOptionsBuilder<ToDoListContext>();
+      var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+      builder.UseMySql(connectionString);
+
+      return new TreatShopContext(builder.Options);
+    }
+  }
+}
